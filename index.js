58776f7dc79adc33676993c80b3c2f24bb67488f
siddharthan21 <siddharthan44@gmail.com>
2023-08-19 22:24:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelector(".hiden").addEventListener("click",function(){
+    document.querySelector(".scrol-left").style.display="flex";
+    // console.log("ha")
+});
+document.querySelector(".hide-scroll-left ").addEventListener("click",function(){
+    document.querySelector(".scrol-left").style.display="none";
+    // console.log("haii")
+});
